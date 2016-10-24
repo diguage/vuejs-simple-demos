e02bed08fc976fd0a823db9a9a901c2ac980a12c
diguage <leejun119@gmail.com>
2016-10-24 17:19:02 +0800

    ElementUI报错了啊

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
-// import Element from 'element-ui'
-// import 'element-ui/lib/theme-default/index.css'
-//
-// Vue.use(Element)
+import Element from 'element-ui'
+import 'element-ui/lib/theme-default/index.css'
+
+Vue.use(Element)
 
 // 1. 默认
 // import App from './App.vue'
